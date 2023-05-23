1861ae8deb592cdef87bdd9e5535d5faa642d96f
Koko-OwO <hcamargo03@misena.edu.co>
2023-05-23 13:33:22 -0500

    Primera subida a github

---
+++ script.js
@@ -0,0 +1,43 @@
+const restaurantesPorCiudad = {
+  "bogota": [
+    { nombre: "Restaurante: Ushin Japanese & Grill", direccion: "Dirección: Calle 24a # 57 - 60 Piso 14 Hotel Grand Haytt" },
+    { nombre: "Restaurante: Los Galenos", direccion: "Dirección: Cra. 23 # 93 -23, Localidad de Chapinero" },
+    { nombre: "Restaurante:Santa Fe Cafe & Restaurante", direccion: "Dirección: Calle 26B 4 No. 4-30" },
+  ],
+  "medellin": [
+    { nombre: "Restaurante: Cala Roca", direccion: "Dirección: Carrera 34 5g 62" },
+    { nombre: "Restaurante: Madurando", direccion: "Dirección: Cl. 10 #30-210 C.C. Vizcaya" },
+    { nombre: "Restaurante: Della Nonna Mall del Este", direccion: "Dirección: Carrera 25 # 3-45, Local 193"},
+  ],
+  "cartagena": [
+    { nombre: "Restaurante: Pezetarian", direccion: "Dirección: Carrera 7 #36-19"},
+    { nombre: "Restaurante: Persepolis Gastrobar", direccion: "Dirección: Carrera 2 9 161 Bocagrande Av. San Martín "},
+    { nombre: "Restaurante: Mama's Pizza", direccion: "Dirección: Calle Del Tablon #7-35"},
+  ],
+  
+};
+
+// Función para buscar y mostrar los restaurantes por ciudad
+function buscarRestaurantes() {
+  const ciudad = document.getElementById("ciudad").value;
+  const restaurantes = restaurantesPorCiudad[ciudad];
+  const resultadosDiv = document.getElementById("resultados");
+
+  resultadosDiv.innerHTML = ""; 
+  // Limpiar resultados anteriores
+
+  if (restaurantes) {
+    const listaRestaurantes = document.createElement("ul");
+
+    for (let i = 0; i < 4 && i < restaurantes.length; i++) {
+      const restaurante = restaurantes[i];
+      const restauranteItem = document.createElement("li");
+      restauranteItem.textContent = `${restaurante.nombre} - ${restaurante.direccion}`;
+      listaRestaurantes.appendChild(restauranteItem);
+    }
+
+    resultadosDiv.appendChild(listaRestaurantes);
+  } else {
+    resultadosDiv.textContent = `No se encontraron restaurantes en esa ciudad.`;
+  }
+}
